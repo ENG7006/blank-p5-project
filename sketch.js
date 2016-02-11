3cdb00641e89c9b9f896285bc465a51284dd61af
kellypolasek <fx2446@wayne.edu>
2016-02-11 16:54:14 -0500

    Daily1-Rocket
    Drew a rocket. (week 1)

--- sketch.js
+++ sketch.js
@@ -3,13 +3,10 @@ function setup() {
 }
 
 function draw() {
-  ellipse(250, 400, 350, 100); // draw the car body
-  ellipse(100, 450, 50, 50); // draw back wheel
-  ellipse(100, 450, 30, 30); // draw back hubcap
-  ellipse(400, 450, 50, 50); // draw front wheel
-  ellipse(400, 450, 30, 30); // draw back hubcap
-  ellipse(431, 397, 10, 20); // draw headlight
-  rect(270, 360, 60, 75); // draw front door
-  rect(170, 360, 60, 75); // draw back door
-  ellipse(391, 351, 5, 45); // draw windshield
+  triangle(250, 450, 450, 450, 350, 350); // draw base
+  ellipse(350, 250, 150, 300); // draw main part of rocket
+  triangle(293, 150, 407, 150, 350, 50); // draw top
+  ellipse(360, 200, 25, 25); // draw window
+  ellipse(360, 250, 25, 25); // draw window 2
+  ellipse(360, 300, 25, 25); // draw window 3
 }

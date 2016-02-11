d469371aeea4323db701be1ae7da9b973ebf5cb7
AdenJ86 <Wire154LP!G>
2016-02-11 16:57:51 -0500

    Paste turntable code.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
-var setup = function () {
+function setup() {
+  createCanvas(600, 600); // build us a canvas
+  rect(130, 200, 260, 200); // main unit
+  ellipse(230, 300, 165, 165); // record
+  ellipse(230, 300, 10, 10); // record holder
+  ellipse(340, 250, 30, 30); // stylus base
+  rect(330, 240, 20, 125); // stylus
+  rect(325, 350, 30, 25); // needle
+}
 
-};
+function draw() {
 
-var draw = function () {
-
-};
+}
